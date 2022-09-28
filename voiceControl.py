8e7dcbf0c5c46d21de6c30515e8f0425d562493f
David-Tovmasyan <88345812+David-Tovmasyan@users.noreply.github.com>
2022-09-28 22:32:23 +0200

    speech_recognizing loop, turtle library
    https://david-tovmasyan.github.io/my-CV/
    https://github.com/David-Tovmasyan
    
    5497026034:AAHr1QEONQCYA5DiNnj6uX7vYQEGyVVVwFM - tg bot token
    
    1. Speech recognizing now works in loop, so it doesn't stop after one attempt
    1.1 To stop the program user can say one of these words ("koniec", "wszystko", "end", "finish", "enough")
    2. Added turtle library
    
    Bug
    
    turtle library blocks speech recognizing

--- voiceControl.py
+++ voiceControl.py
@@ -1,21 +1,51 @@
-#import library
-
 import speech_recognition as sr
+from turtle import *
 
-# Initialize recognizer class (for recognizing the speech)
+program_is_running = True  
+
+color('red', 'yellow')
+begin_fill()
+while program_is_running:
+    right(10)
+    forward(10)
+
+ending_words = ["koniec", "wszystko", "end", "finish", "enough"]
+
+PROGRAM_LANGUAGE = "en-UK"  # pl-PL
 
 r = sr.Recognizer()
 
-# Reading Microphone as source
-# listening the speech and store in audio_text variable
+def checking_word(text):
 
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
-    print("Talk")
-    audio_text = r.listen(source)
-    
-    try:
-        # using google speech recognition
-        print("Text: "+r.recognize_google(audio_text,language="pl-PL"))
-    except:
-         print("Sorry, I did not get that")
+    global program_is_running
+
+    text_arr = text.split(" ")
+
+    for word in text_arr:
+        if word.lower() in ending_words:
+            program_is_running = False
+            end_fill()
+            done()
+        #data variables should be changed into 'word'_words, because turtle library has functions like right(), left(), forward()
+        # elif word in right_words: 
+        #     return "Rigth"
+        # elif word in left_words:
+        #     return "Left"
+        # elif word in front_words:
+        #     return "Front"
+        # else: return "Nothing"
+
+
+while program_is_running:
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("Talk")
+        audio_text = r.listen(source)
+
+        try:
+            recognized_text = r.recognize_google(audio_text, language=PROGRAM_LANGUAGE)
+            print("Text: "+recognized_text)
+            checking_word(recognized_text)
+
+        except:
+            print("Sorry, I did not get that")

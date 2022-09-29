ba6d1f22b9c80e449785b8da0f3d412d959b2813
David-Tovmasyan <88345812+David-Tovmasyan@users.noreply.github.com>
2022-09-29 15:39:30 +0200

    Voice Recoginition now works with turtle
    voice commands can draw using turtle library

---
+++ data.py
@@ -0,0 +1,6 @@
+front_words  = ["prosto","w przód", "naprzód", "front","ahead"]
+left_words   = ["lewo", "left"]
+right_words  = ["prawo", "right"]
+ending_words = ["koniec", "wszystko", "end", "finish", "enough","stop"]
+
+PROGRAM_LANGUAGE = "en-UK"  # pl-PL
--- voiceControl.py
+++ voiceControl.py
@@ -1,17 +1,12 @@
 import speech_recognition as sr
 from turtle import *
+import data
 
 program_is_running = True  
 
 color('red', 'yellow')
 begin_fill()
-while program_is_running:
-    right(10)
-    forward(10)
-
-ending_words = ["koniec", "wszystko", "end", "finish", "enough"]
-
-PROGRAM_LANGUAGE = "en-UK"  # pl-PL
+left(90)
 
 r = sr.Recognizer()
 
@@ -20,22 +15,19 @@ def checking_word(text):
     global program_is_running
 
     text_arr = text.split(" ")
-
+    
     for word in text_arr:
-        if word.lower() in ending_words:
+        if word in data.ending_words:
             program_is_running = False
             end_fill()
             done()
-        #data variables should be changed into 'word'_words, because turtle library has functions like right(), left(), forward()
-        # elif word in right_words: 
-        #     return "Rigth"
-        # elif word in left_words:
-        #     return "Left"
-        # elif word in front_words:
-        #     return "Front"
-        # else: return "Nothing"
-
-
+        elif word in data.right_words: 
+            forward(10)
+            right(10)
+        elif word in data.left_words:
+            forward(10)
+            left(10)                 
+        
 while program_is_running:
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
@@ -43,9 +35,10 @@ while program_is_running:
         audio_text = r.listen(source)
 
         try:
-            recognized_text = r.recognize_google(audio_text, language=PROGRAM_LANGUAGE)
+            recognized_text = r.recognize_google(audio_text, language=data.PROGRAM_LANGUAGE)
             print("Text: "+recognized_text)
             checking_word(recognized_text)
 
         except:
             print("Sorry, I did not get that")
+
